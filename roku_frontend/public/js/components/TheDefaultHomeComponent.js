55b4573c5f4297e8b0078ba7442461ff332f8a99
evica-ai <e_mai@fanshaweonline.ca>
2023-04-10 15:43:11 -0400

    frontend

---
+++ roku_frontend/public/js/components/TheDefaultHomeComponent.js
@@ -0,0 +1,15 @@
+export default {
+    name: 'TheDefaultHomeComponent',
+
+    props: ['user'],
+
+    template: `
+    <h1> The Default Home Component </h1>
+    `,
+
+    created() {
+        // fetch the appropriate video data from the IMDB API
+        // and load it into your view -> Watch the youtube video
+            
+    }
+}
